cd1cefa7c2687087960266efca91c22ba176b499
martynaut <martyna.urbanek@gmail.com>
2020-06-06 22:01:35 +0200

    clean repository

--- new_coord_run.sh
+++
@@ -1,8 +0,0 @@
-python3 run_mirnaome_analysis.py /Users/martynaurbanek/Documents/private/repos/files/mirnaome_input/LUAD \
-/Users/martynaurbanek/Documents/private/repos/files/output/DATA_LUAD_new_coord \
-/Users/martynaurbanek/Documents/private/repos/files/file_for_mirnaome_test2/coordinates.bed \
-/Users/martynaurbanek/Documents/private/repos/files/files_for_mirnaome/confidence_file.xlsx \
-/Users/martynaurbanek/Documents/private/repos/files/files_for_mirnaome/hsa.gff \
-/Users/martynaurbanek/Documents/private/repos/files/files_for_mirnaome/cancer_exons.txt \
-/Users/martynaurbanek/Documents/private/repos/files/file_for_mirnaome_test2/localizations_test.csv \
-/Users/martynaurbanek/Documents/private/repos/files/files_for_mirnaome/mirbase_count.csv -s 2
